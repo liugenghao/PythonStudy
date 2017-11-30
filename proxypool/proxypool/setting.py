053976dc2a054414ae4525b6c879febef75fae4b
liugenghao <liugenghao@gmail.com>
2017-11-30 19:50:15 +0800

    u

--- proxypool/proxypool/setting.py
+++ proxypool/proxypool/setting.py
@@ -13,20 +13,14 @@ POOL_LOWER_THRESHOLD = 100
 POOL_UPPER_THRESHOLD = 1000
 
 # 检查周期
-<<<<<<< HEAD
 # VALID_CHECK_CYCLE = 60
 # POOL_LEN_CHECK_CYCLE = 20
-=======
->>>>>>> 864c253a27f57eb3640978d9606349bba4a2fb89
+
 VALID_CHECK_CYCLE = 3600
 POOL_LEN_CHECK_CYCLE = 20
 
 # 测试API，用百度来测试
-<<<<<<< HEAD
 # TEST_API='http://www.baidu.com'
 # TEST_API='http://www.sogou.com'
 TEST_API='http://weixin.sogou.com/'
-=======
-TEST_API='http://www.baidu.com'
-# TEST_API='http://www.sogou.com'
->>>>>>> 864c253a27f57eb3640978d9606349bba4a2fb89
+
